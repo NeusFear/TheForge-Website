44ea08f8cd6e89954504f7232664640075e16c89
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-04 11:48:51 -0500

    Update

--- home.php
+++ home.php
@@ -20,8 +20,7 @@
     <link href="css/style.css" rel="stylesheet"></link>
 
       <!--Scripts-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="scripts/parallax.js"></script>
+    <script src="bot.js"></script>
   </head>
 
 
