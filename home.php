67e94a5d0b6213578eb8f786a35b7afb51db16de
SkyForge_ <goldr1000ftw@gmail.com>
2017-08-10 18:51:57 -0500

    test

--- home.php
+++ home.php
@@ -37,7 +37,7 @@
         <div class="splash-left"><font color="#ffc342">Who are we?</font><br /></div>
         </div>
       <div class="transition">
-        <p><Small>We are a new modern gaming community that aims to make a fun and helpful experience for today's video game loving community.</small></p>
+        We are a new modern gaming community that aims to make a fun and helpful experience for today's video game loving community.
       </div>
 
       <div class="parallax" id="parallax-3"></div>
