71059e5b018189249f8f257fc9779ca00f089226
GoldrDragon <alex@houpt.us>
2017-08-16 09:50:35 -0500

    this was also justified

--- home.php
+++ home.php
@@ -36,7 +36,7 @@
       <div class="transition">
         <div class="splash-left"><font color="#ffc342">Who are we?</font><br /></div>
         <p>We are a new modern gaming community that aims to make a fun and helpful experience for today's video game loving world.</p>
-        <justify>
+        <center>
           <div class="info" style="margin-left: 20%;"><img src="images/Icons/circular/success.png" /><br />Modern<p>We aim to keep at the forefront of developments in gaming.</p></div>
           <div class="info"><img src="images/Icons/circular/list.png" /><br />Forums<p>To keep progressive gamers connected in a like-minded community.</p></div>
           <div class="info"><img src="images/Icons/circular/internet.png" /><br />Servers<p>Our community hosts dedicated servers avalible to anyone.</p></div>
@@ -44,7 +44,7 @@
           <div class="info" style="margin-left: 20%;"><img src="images/Icons/circular/volume.png" /><br />Discord<p>The fastest way to communicate with other members, and recieve updates on server information.</p></div>
           <div class="info"><img src="images/Icons/circular/email.png" /><br />Staff<p>Our helpful staff endeavors to resolve all issues and answer all questions as quickly and helpfully as possible.</p></div>
           <div class="info"><img src="images/Icons/circular/earth-globe.png" /><br />Community<p>The main goal of our community is to enjoy the games we play as well as provide helpful feedback on those games.</p></div>
-        </justify>
+        </center>
       </div>
 
       <div class="parallax" id="parallax-2"></div>
