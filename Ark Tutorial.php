74cb4a15b0c9138989d1253d0bcc90d3f66b7a78
GoldrDragon <alex@houpt.us>
2017-08-16 11:21:33 -0500

    ArkTutorial

---
+++ Ark Tutorial.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8"></meta>
+      <!--Tab Title-->
+    <title>Adding Ark</title>
+
+      <!--Meta-->
+    <meta content="The Forge - Minecraft Network" name="description"></meta>
+    <meta content="SkyForge_" name="author"></meta>
+
+      <!--Favicon-->
+    <link href="images/icons/favicon.ico" rel="shortcut icon"></link>
+
+      <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"></link>
+
+      <!--Styles-->
+    <link href="css/style.css" rel="stylesheet"></link>
+
+      <!--Scripts-->
+    <script src="bot.js"></script>
+    <script src="js/main.js"></script>
+  </head>
+
+<html>
