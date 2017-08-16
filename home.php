dce5414d7fd659d691332bc60dd6dfc09127b0f5
GoldrDragon <alex@houpt.us>
2017-08-16 09:21:43 -0500

    shorteninging

--- home.php
+++ home.php
@@ -37,7 +37,7 @@
         <div class="splash-left"><font color="#ffc342">Who are we?</font><br /></div>
         <p>We are a new modern gaming community that aims to make a fun and helpful experience for today's video game loving world.</p>
         <center>
-          <div class="info" style="margin-left: 20%;"><img src="images/Icons/circular/success.png" /><br />Modern<p>We aim to keep at the forefront of developments in gaming whether that be hardware or software, to give you the best, and most up-to-date experi0ence possible.</p></div>
+          <div class="info" style="margin-left: 20%;"><img src="images/Icons/circular/success.png" /><br />Modern<p>We aim to keep at the forefront of developments in gaming.</p></div>
           <div class="info"><img src="images/Icons/circular/list.png" /><br />Forums<p>To keep progressive gamers connected in a like-minded community.</p></div>
           <div class="info"><img src="images/Icons/circular/internet.png" /><br />Servers<p>Our community hosts dedicated servers avalible to anyone.</p></div>
           <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
